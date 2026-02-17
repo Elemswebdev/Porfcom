7e72e32ebd39af90a9e51365db45edaf2cbc969e
ElemsWebdev <elemsikwegbupromise@gmail.com>
2026-02-17 13:14:51 +0100

    svgs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,47 @@
-import React from "react"
-import type { Metadata, Viewport } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import React from "react";
+import type { Metadata, Viewport } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import "./globals.css";
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const viewport: Viewport = {
-  themeColor: '#3b82f6',
-  width: 'device-width',
+  themeColor: "#3b82f6",
+  width: "device-width",
   initialScale: 1,
   userScalable: true,
-}
+};
 
 export const metadata: Metadata = {
-  title: 'Porfcom - Raising a people of faith, power, and purpose',
-  description: 'Welcome to Porfcom, a modern church community dedicated to raising a people of faith, power, and purpose',
-  generator: 'v0.app',
+  title: "Porfcom - Raising a people of faith, power, and purpose",
+  description:
+    "Welcome to Porfcom, a modern church community dedicated to raising a people of faith, power, and purpose",
+  // generator: 'v0.app',
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
+        url: "/porfcom logo-cropped.png",
+        media: "(prefers-color-scheme: light)",
       },
       {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
+        url: "/porfcom logo-cropped.png",
+        media: "(prefers-color-scheme: dark)",
       },
       {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: "porfcom logo-cropped.png",
+        type: "image/svg+xml",
       },
     ],
-    apple: '/apple-icon.png',
+    apple: "/porfcom logo-cropped.png",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -49,5 +50,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
